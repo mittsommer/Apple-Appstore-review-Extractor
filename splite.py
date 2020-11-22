264883ed6f5fa23d885fa688cad6f7d73553ab20
Kai Liang <liangkai0625@outlook.com>
2020-11-22 22:43:53 +0100

    Delete splite.py

--- splite.py
+++
@@ -1,8 +0,0 @@
-import MeCab
-def splite(review):
-    review_splite = []
-    for i in range(len(review)):
-        mecab_tagger = MeCab.Tagger("-Owakati")
-        rs = mecab_tagger.parse(review[i])
-        review_splite.append(rs)
-    return review_splite
